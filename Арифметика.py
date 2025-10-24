6678f1dd67e5cb71ae808198bcd3641eab0c4588
katinkosty269 <katinkosty269@gmail.com>
2025-10-24 20:45:43 +0300

    Арифметические операции

---
+++ Арифметика.py
@@ -0,0 +1,13 @@
+from math import *
+
+a = 17
+b = 3
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b) # деление дробное
+print(a // b) #целочисленное деление
+print(a % b) #остаток от деления
+print(a ** b)
+print(9**(1/2)) #корень
+print(int(sqrt(9)))
