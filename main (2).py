026b9fc6d23e68062d9deccb2d9cfa177f711974
katinkosty269 <katinkosty269@gmail.com>
2025-10-25 18:09:22 +0300

    ТИП 5 %15974

---
+++ main (2).py
@@ -0,0 +1,12 @@
+#ТИп 5 №15974
+for N in range(10000,1, -1):
+    s = bin(N)[2:]
+    if N % 2 == 0:
+        s += '10'
+    else:
+        s += '01'
+    R = int(s, 2)
+    if R <= 102:
+        print(R)
+        break
+    
